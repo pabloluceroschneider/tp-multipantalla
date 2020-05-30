d41d55f1c3b9e95acc82f9856352773990499002
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-30 13:43:39 -0300

    Initialize Navigation

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -11,7 +11,7 @@ const PhotoList = (props) => {
   useEffect(() => {
     const fetchphotos = async () => {
       const data = await axios.get(
-        `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`
+        `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`
       );
       setPhotos(data.data.photoset.photo);
     };
