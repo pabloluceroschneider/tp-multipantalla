cb2a60cf74c1eeecfa8bb7be699d0f516ab52b51
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-30 11:34:23 -0300

    Agrego async en useEffect

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -8,8 +8,10 @@ const AlbumList = (props) => {
 	const [ photoset, setPhotoset ] = useState(null);
 
 	useEffect( ()=>{
-	  axios.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1')
-	    .then(response => setPhotoset(response.data.photosets.photoset))
+		const fetchalbum = async () => {
+			await axios.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1')
+			.then(response => setPhotoset(response.data.photosets.photoset))}
+		fetchalbum()
 	},[]);
 
 	return !photoset ? (
