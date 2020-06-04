b2fb69a8263ed3426e3c60e8275cdf5ef5937da6
BrunoConti <bruno.conti1997@gmail.com>
2020-06-04 20:42:39 -0300

    fix: albumlist styles

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,44 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
-import axios from 'axios';
-import AlbumDetail from './AlbumDetail';
-import {FlatList} from 'react-native';
-import Loader from './Loader';
-
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
+import { FlatList } from "react-native";
+import Loader from "./Loader";
 
 const AlbumList = (props) => {
-	const [ photoset, setPhotoset ] = useState(null);
+  const [photoset, setPhotoset] = useState(null);
 
-	useEffect( ()=>{
-		const fetchalbum = async () => {
-			await axios.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1')
-			.then(response => setPhotoset(response.data.photosets.photoset))}
-		fetchalbum()
-	},[]);
+  useEffect(() => {
+    const fetchalbum = async () => {
+      await axios
+        .get(
+          "https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1"
+        )
+        .then((response) => setPhotoset(response.data.photosets.photoset));
+    };
+    fetchalbum();
+  }, []);
 
-	return !photoset ? (
-		<Loader size="large" />
-	) : (
-		<View style={styles}>
-			<FlatList
-				data={photoset}
-				renderItem={({ item }) =>  <AlbumDetail 
-													navigation={props.navigation}
-													key={item.id} 
-													title={item.title._content} 
-													albumId={item.id} />}
-				keyExtractor={(item) => item.id}
-			/>
-		</View>
-	);
+  return !photoset ? (
+    <Loader size="large" />
+  ) : (
+    <View style={styles}>
+      <FlatList
+        data={photoset}
+        renderItem={({ item }) => (
+          <AlbumDetail
+            navigation={props.navigation}
+            key={item.id}
+            title={item.title._content}
+            albumId={item.id}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 };
 
 const styles = {
-	flex: 1,
-	marginTop: "1rem",
-	marginRight: "1rem",
-	marginBottom: "5rem",
-	marginLeft: "1rem"
-}
+  flex: 1,
+  marginTop: "1%",
+  marginRight: "1%",
+  marginBottom: "1%",
+  marginLeft: "1%",
+};
 
 export default AlbumList;
